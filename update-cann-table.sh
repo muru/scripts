ac8975f0ef139e0a139ab3856de76eb0b5165ac4
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2019-10-19 17:09:15 +0000

    fix typo

--- update-cann-table.sh
+++ update-cann-table.sh
@@ -2,9 +2,8 @@
 set -xue
 PATH="$HOME/bin:$PATH"
 
-cd ~/devel/web
+cd ~/dev/web
 git pull
 ~/bin/table.sh | ~/bin/cann-table.py > cann-table.html
-git commit -am 'updated cann-table: $(date)'
-git push cse
+git commit -am "updated cann-table: $(date)"
 git push origin
