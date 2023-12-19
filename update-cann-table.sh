70ed1083165a00b96c32eaa94e95a5b7840832c0
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2023-12-19 19:27:20 +0900

    Cache tables to avoid regenerating if no changes

--- update-cann-table.sh
+++ update-cann-table.sh
@@ -4,6 +4,13 @@ PATH="$HOME/bin:$PATH"
 
 cd ~/dev/web
 git pull
-~/bin/table.sh | ~/bin/cann-table.py > cann-table.html
+~/bin/table.sh > ~/.cache/cann-table-new.csv
+if diff -q ~/.cache/cann-table-new.csv ~/.cache/cann-table.csv
+then
+	echo No changes in the PL table.
+	exit
+fi
+mv ~/.cache/cann-table-new.csv ~/.cache/cann-table.csv
+~/bin/cann-table.py < ~/.cache/cann-table.csv > cann-table.html
 git commit -am "updated cann-table: $(date)"
 git push origin
